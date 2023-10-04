22bcaba3f0580ab15bcad4c591f2e15b7ac6f742
sejal99 <sejal8974@gmail.com>
2023-10-04 14:00:00 +0100

    new commit

--- client/src/components/Page.jsx
+++ client/src/components/Page.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
 
-const Page = () => {
-    const [users, setUsers]= useState([]);
-    console.log(users.email);
-    const getData= async()=> {
-        try{
-            const res= await axios.get('https://reqres.in/api/users?page=1');
+// const Page = () => {
+//     const [users, setUsers]= useState([]);
+//     console.log(users.email);
+//     const getData= async()=> {
+//         try{
+//             const res= await axios.get('https://reqres.in/api/users?page=1');
           
-            setUsers(res.data.data)
+//             setUsers(res.data.data)
 
 
-        }catch(err){
-            console.log(err);
-        }
+//         }catch(err){
+//             console.log(err);
+//         }
      
 
-    }
+//     }
 
-    useEffect(()=> {
-        getData();
-    },[]);
-    if(!users){
-       return <h1>Loading......</h1>
-    }
-    console.log(users.data);
+//     useEffect(()=> {
+//         getData();
+//     },[]);
+//     if(!users){
+//        return <h1>Loading......</h1>
+//     }
+//     console.log(users.data);
 
-  return (
-    <div>
-        {users?.map((user)=> (
-            <div key={user.id}>
-                <img src={user.avatar} alt="" />
-                <div style={{display:"flex",gap:"10px"}}>
-                <div>{user.first_name} </div>
-                <div> {user.last_name} </div>
+//   return (
+//     <div>
+//         {users?.map((user)=> (
+//             <div key={user.id}>
+//                 <img src={user.avatar} alt="" />
+//                 <div style={{display:"flex",gap:"10px"}}>
+//                 <div>{user.first_name} </div>
+//                 <div> {user.last_name} </div>
                
                 
-                </div>
+//                 </div>
 
-                <div>{user.email}</div>
+//                 <div>{user.email}</div>
 
-            </div>
-        ))}
+//             </div>
+//         ))}
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default Page
+// export default Page
