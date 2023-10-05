f0600b1375a7154cc9daa57d6e0eee9d0de99674
sejal99 <sejal8974@gmail.com>
2023-10-05 18:25:45 +0530

    new commit

--- client/src/components/Page.jsx
+++ client/src/components/Page.jsx
@@ -1,50 +1,50 @@
-// import React, { useEffect, useState } from 'react'
-// import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-// const Page = () => {
-//     const [users, setUsers]= useState([]);
-//     console.log(users.email);
-//     const getData= async()=> {
-//         try{
-//             const res= await axios.get('https://reqres.in/api/users?page=1');
+const Page = () => {
+    const [users, setUsers]= useState([]);
+    console.log(users.email);
+    const getData= async()=> {
+        try{
+            const res= await axios.get('https://reqres.in/api/users?page=1');
            
-//             setUsers(res.data)
+            setUsers(res.data)
 
 
-//         }catch(err){
-//             console.log(err);
-//         }
-//        const res= await axios.get('https://reqres.in/api/users?page=1');
+        }catch(err){
+            console.log(err);
+        }
+       const res= await axios.get('https://reqres.in/api/users?page=1');
 
-//     }
+    }
 
-//     useEffect(()=> {
-//         getData();
-//     },[]);
-//     // if(!users.email){
-//     //    return <h1>Loading......</h1>
-//     // }
-//     console.log(users.email);
+    useEffect(()=> {
+        getData();
+    },[]);
+    // if(!users.email){
+    //    return <h1>Loading......</h1>
+    // }
+    console.log(users.email);
 
-//   return (
-//     <div>
-//         {users.map((user)=> (
-//             <div key={user.id}>
-//                 <img src={user.avatar} alt="" />
-//                 <div style={{display:"flex",gap:"10px"}}>
-//                 <div>{user.first_name} </div>
-//                 <div> {user.last_name} </div>
+  return (
+    <div>
+        {users.map((user)=> (
+            <div key={user.id}>
+                <img src={user.avatar} alt="" />
+                <div style={{display:"flex",gap:"10px"}}>
+                <div>{user.first_name} </div>
+                <div> {user.last_name} </div>
                
                 
-//                 </div>
+                </div>
 
-//                 <div>{user.email}</div>
+                <div>{user.email}</div>
 
-//             </div>
-//         ))}
+            </div>
+        ))}
       
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default Page
+export default Page
