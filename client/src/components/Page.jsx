d99b4cc3ee5fbebd31ad542fcb16cff3e48d959c
sejal99 <sejal8974@gmail.com>
2023-10-05 19:19:05 +0530

    new commit

--- client/src/components/Page.jsx
+++ client/src/components/Page.jsx
@@ -7,28 +7,28 @@ const Page = () => {
     const getData= async()=> {
         try{
             const res= await axios.get('https://reqres.in/api/users?page=1');
-           
-            setUsers(res.data)
+          
+            setUsers(res.data.data)
 
 
         }catch(err){
             console.log(err);
         }
-       const res= await axios.get('https://reqres.in/api/users?page=1');
+     
 
     }
 
     useEffect(()=> {
         getData();
     },[]);
-    // if(!users.email){
-    //    return <h1>Loading......</h1>
-    // }
-    console.log(users.email);
+    if(!users){
+       return <h1>Loading......</h1>
+    }
+    console.log(users.data);
 
   return (
     <div>
-        {users.map((user)=> (
+        {users?.map((user)=> (
             <div key={user.id}>
                 <img src={user.avatar} alt="" />
                 <div style={{display:"flex",gap:"10px"}}>
