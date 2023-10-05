63392e1dccdf84c9d4d4b3b7830b234037f6945d
sejal99 <sejal8974@gmail.com>
2023-10-05 18:26:24 +0530

    new commit

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,40 +1,40 @@
-// import React from 'react'
-// import { Link, useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 
-// const Signin = () => {
-//     const navigate=useNavigate();
-//     const handleSubmit=()=>{
-//         navigate('/page')
-//     }
-//   return (
-// <div>
-//       <form onSubmit={handleSubmit}>
-//       <div style={{height:"90vh", display:"flex",flexDirection:"column", alignItems:"center",justifyContent:"center",gap:"10px"}} >
-//     <h1>Signin Here</h1>
-//       <div style={{display:"flex",alignItems:"center",flexDirection:"column",gap:"10px"}}>
+const Signin = () => {
+    const navigate=useNavigate();
+    const handleSubmit=()=>{
+        navigate('/page')
+    }
+  return (
+<div>
+      <form onSubmit={handleSubmit}>
+      <div style={{height:"90vh", display:"flex",flexDirection:"column", alignItems:"center",justifyContent:"center",gap:"10px"}} >
+    <h1>Signin Here</h1>
+      <div style={{display:"flex",alignItems:"center",flexDirection:"column",gap:"10px"}}>
         
-//       <div >
-//       <input  style={{padding:"10px",borderRadius:"10px"}}type='email'  placeholder='Username' />
-//       </div>
+      <div >
+      <input  style={{padding:"10px",borderRadius:"10px"}}type='email'  placeholder='Username' />
+      </div>
 
-//       <div>
-//       <input style={{padding:"10px",borderRadius:"10px"}} type='password'  placeholder='password'/>
-//       </div>
+      <div>
+      <input style={{padding:"10px",borderRadius:"10px"}} type='password'  placeholder='password'/>
+      </div>
 
-//       <div>
-//       <button type='Submit' style={{padding:"10px",borderRadius:"10px"}}>Signin</button>
-//       </div>
-//       </div>
-//       <div>Not having Account? <Link to={'/signup'}>Signup</Link> to continue</div>
-//       </div>
+      <div>
+      <button type='Submit' style={{padding:"10px",borderRadius:"10px"}}>Signin</button>
+      </div>
+      </div>
+      <div>Not having Account? <Link to={'/signup'}>Signup</Link> to continue</div>
+      </div>
 
-//       </form>
+      </form>
       
     
 
-//       </div>
-//   )
-// }
+      </div>
+  )
+}
 
-// export default Signin
+export default Signin
