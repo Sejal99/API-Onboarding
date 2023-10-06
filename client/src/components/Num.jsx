4ee40167a926744aa362efc6f53a2e2d41644c3e
sejal99 <sejal8974@gmail.com>
2023-10-06 21:42:18 +0530

    new commit

--- client/src/components/Num.jsx
+++ client/src/components/Num.jsx
@@ -1,19 +1,19 @@
-// import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
-// const Num = () => {
-//     const [count,setCount]=useState(0)
-//     useEffect(()=>{
-//         document.title=`count ${count} times`
-//     })
-//   return (
-//     <div>
-//       <button onClick={()=>{
-//         setCount(count+1);
-//       }}>Click 0 times</button>
-//       count {count}times
-//     </div>
-//   )
-// }
+const Num = () => {
+    const [count,setCount]=useState(0)
+    useEffect(()=>{
+        document.title=`count ${count} times`
+    })
+  return (
+    <div>
+      <button onClick={()=>{
+        setCount(count+1);
+      }}>Click 0 times</button>
+      count {count}times
+    </div>
+  )
+}
 
-// export default Num
+export default Num
