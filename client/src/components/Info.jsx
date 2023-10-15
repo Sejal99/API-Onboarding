be7667ec6c5df819ea9f2c77d06c8722afa261ac
sejal99 <sejal8974@gmail.com>
2023-10-15 18:55:51 +0530

    new commit

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -1,25 +1,25 @@
-// import React from 'react'
-// import { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
 
-// const Info = () => {
-//     const [userName,setUserName]=useState({firstname:'', lastname:''})
+const Info = () => {
+    const [userName,setUserName]=useState({firstname:'', lastname:''})
 
 
 
-//   return (
-//     <div>
-//       <input  type='string' onChange={(e)=>{
-//         setUserName({...userName,firstname: e.target.value});
-//       }} placeholder='firstname'/>
-//       <input onChange={(e)=>{
-//         setUserName({...userName,lastname:e.target.value});
-//       }} type='string' placeholder='lastname'/>
-//       <div>
-//         <h1>My first is- {userName.firstname} </h1>
-//         <h1>My last is- {userName.lastname} </h1>
-//       </div>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      <input  type='string' onChange={(e)=>{
+        setUserName({...userName,firstname: e.target.value});
+      }} placeholder='firstname'/>
+      <input onChange={(e)=>{
+        setUserName({...userName,lastname:e.target.value});
+      }} type='string' placeholder='lastname'/>
+      <div>
+        <h1>My first is- {userName.firstname} </h1>
+        <h1>My last is- {userName.lastname} </h1>
+      </div>
+    </div>
+  )
+}
 
-// export default Info
+export default Info
