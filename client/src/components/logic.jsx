fd31adcee98b41bc74db0fd8e7f307834d2b4af3
sejal99 <sejal8974@gmail.com>
2023-10-21 21:58:08 +0530

    new commit

--- client/src/components/logic.jsx
+++ client/src/components/logic.jsx
@@ -1,59 +1,59 @@
-// import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-// const posts= [
-//   {id:1, title:"delhi", desc:"capital"},
-//   {id:2, title:"india", desc:"country"},
-//   {id:3, title:"nainital", desc:"district"},
-//   {id:4, title:"pakistan" , desc:"country"},
-//   {id:5, title:"ara" , desc:"district"},
+const posts= [
+  {id:1, title:"delhi", desc:"capital"},
+  {id:2, title:"india", desc:"country"},
+  {id:3, title:"nainital", desc:"district"},
+  {id:4, title:"pakistan" , desc:"country"},
+  {id:5, title:"ara" , desc:"district"},
   
-// ]
+]
 
 
-// const Logic = () => {
-//   const[post,setPost]=useState([])
-//   const[value,setValue]=useState('')
-//   const [ans,setAns]=useState([])
+const Logic = () => {
+  const[post,setPost]=useState([])
+  const[value,setValue]=useState('')
+  const [ans,setAns]=useState([])
  
   
-//   useEffect(()=>{
-//     const fun= ()=> {
-//       setPost(posts)
-//     }
+  useEffect(()=>{
+    const fun= ()=> {
+      setPost(posts)
+    }
   
-//     fun()
-//   },[ans])
+    fun()
+  },[ans])
 
-//   console.log(ans);
+  console.log(ans);
 
-//   return (
-//     <div>
-//    <input onChange={(e)=>{
-//     setValue(e.target.value)
-//    }} type='string' placeholder='string'/>
+  return (
+    <div>
+   <input onChange={(e)=>{
+    setValue(e.target.value)
+   }} type='string' placeholder='string'/>
 
-//    <button onClick={()=>{
-//   let arr= []
-//     post.map((p)=>{
+   <button onClick={()=>{
+  let arr= []
+    post.map((p)=>{
       
-//              if(p.desc===(value)){
-//              arr.push(p)
-//         }
+             if(p.desc===(value)){
+             arr.push(p)
+        }
        
        
-//    })
-//    setAns(arr)
-//    }}>Click Me</button>
+   })
+   setAns(arr)
+   }}>Click Me</button>
   
-//    {ans.map((a)=>(
-//     <div key={a.id}>
-//         {a.title}
-//     </div>
-//    )
+   {ans.map((a)=>(
+    <div key={a.id}>
+        {a.title}
+    </div>
+   )
 
-//    )}
-//     </div> 
-//   )
-// }
+   )}
+    </div> 
+  )
+}
 
-// export default Logic
+export default Logic
